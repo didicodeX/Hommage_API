c689b9b8e18eb2ea705b7a95c1b6e8543424d191
Dylane <dylane@didicode.com>
2025-08-25 06:36:25 -0600

    feat: filter donations by status and update statistics calculations

--- src/features/donation/donation.repository.js
+++ src/features/donation/donation.repository.js
@@ -1,21 +1,22 @@
-import  Donation  from "./donation.model.js";
+import Donation from "./donation.model.js";
 
 export const getAllDonations = async () => {
-  return await Donation.find().sort({ createdAt: -1 });
+  return await Donation.find({ status: "paid" }).sort({ createdAt: -1 });
 };
 
 export const getRecentDonations = async (limit = 5) => {
-  return await Donation.find()
+  return await Donation.find({ status: "paid" })
     .sort({ createdAt: -1 })
     .limit(limit);
 };
 
 export const getDonationStats = async () => {
   const totalAmount = await Donation.aggregate([
+    { $match: { status: "paid" } },
     { $group: { _id: null, total: { $sum: "$amount" } } },
   ]);
 
-  const totalCount = await Donation.countDocuments();
+  const totalCount = await Donation.countDocuments({ status: "paid" });
 
   const GOAL_AMOUNT = 1000; // Valeur cible fixe ou récupérée d'ailleurs
 
